2b7f832bfa1f207ebfe0282d8be11295a07bac50
Patrick Vanegas <pvanny1124@gmail.com>
2018-03-09 00:11:54 -0500

    update question 8 query

--- exercise2.sql
+++ exercise2.sql
@@ -57,8 +57,14 @@ Years'. Use a join */
 
 SELECT 'List the store\'s name and the order number in salesdetail for all stores that bought title including "Fifty
 Years". Use a join' AS 'Question 8';
+SELECT 'SELECT stores.stor_name salesdetail.ord_num FROM ((titles ti INNER JOIN salesdetail sa ON ti.title LIKE '%Fifty Years%' AND ti.title_id = sa.title_id)) INNER JOIN stores on sa.stor_id = stores.stor_id;' AS 'Query';
+SELECT stores.stor_name salesdetail.ord_num FROM 
+((titles ti INNER JOIN salesdetail sa ON ti.title LIKE '%Fifty Years%' AND ti.title_id = sa.title_id)) 
+      INNER JOIN stores on sa.stor_id = stores.stor_id; 
 
-SELECT stores.stor_name, stores_bought.ord_num FROM stores JOIN (SELECT stor_id, ord_num FROM salesdetail WHERE title_id=(SELECT title_id FROM titles WHERE title LIKE "%Fifty Years%")) AS stores_bought ON stores.stor_id = stores_bought.stor_id;
+/* Redundant code: */
+/* SELECT stores.stor_name, stores_bought.ord_num FROM stores JOIN (SELECT stor_id, ord_num FROM salesdetail WHERE title_id=(SELECT title_id FROM titles WHERE title LIKE "%Fifty Years%")) AS stores_bought ON stores.stor_id = stores_bought.stor_id;
+*/
 
 /* What book (actual title and author) is most ordered (qty) by bookstores in salesdetail? Identify the
 output with an "as" clause. Use a join. */
